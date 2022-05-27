3872875eee88202100891e2cba247573e1de010f
rhnh <rhnh@protonmail.com>
2022-05-27 16:14:54 +0200

    if 0 zero list display a message

--- src/screens/lists/ListItems.tsx
+++ src/screens/lists/ListItems.tsx
@@ -5,33 +5,37 @@ import { Link } from 'react-router-dom'
 export const ListItems: FC<{ birds: any[] }> = ({ birds }) => {
   return (
     <>
-      {birds?.map(bird => (
-        <div
-          key={bird._id}
-          className={css({
-            padding: '1em',
-            border: '1px solid gray',
-            display: 'flex',
-            marginBottom: '3px',
-            flexDirection: 'column',
-          })}
-        >
-          <p>
-            Name:{' '}
-            <Link
-              to={`/taxonomies/taxonomy/id/${bird._id
-                ?.toLowerCase()
-                .replace(/[^a-z0-9]+/, '-')}`}
-            >
-              {bird.englishName}
-            </Link>
-          </p>
-          <p>
-            Added to your list on:{' '}
-            <strong> {new Date(bird.createdAt).toDateString()}</strong>
-          </p>
-        </div>
-      ))}
+      {birds.length > 0 ? (
+        birds?.map(bird => (
+          <div
+            key={bird._id}
+            className={css({
+              padding: '1em',
+              border: '1px solid gray',
+              display: 'flex',
+              marginBottom: '3px',
+              flexDirection: 'column',
+            })}
+          >
+            <p>
+              Name:{' '}
+              <Link
+                to={`/taxonomies/taxonomy/id/${bird._id
+                  ?.toLowerCase()
+                  .replace(/[^a-z0-9]+/, '-')}`}
+              >
+                {bird.englishName}
+              </Link>
+            </p>
+            <p>
+              Added to your list on:{' '}
+              <strong> {new Date(bird.createdAt).toDateString()}</strong>
+            </p>
+          </div>
+        ))
+      ) : (
+        <p>No bird found</p>
+      )}
     </>
   )
 }
